19921763fdbb3a8d84847d8879753adfc46a383e
sanku <san.kukkonen@gmail.com>
2022-02-03 11:06:39 +0200

    sannan kommentti 3 mainiin

--- src/main/java/r8/App.java
+++ src/main/java/r8/App.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 /**
  * JavaFX App
  * 
- * Sannan testikommentti vielä mainiin 2
+ * Sannan testikommentti vielä mainiin 3
  * 
  */
 public class App extends Application {
