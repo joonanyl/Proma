f1befc01c5c15c65f5cf97b4bd195c91d4e64216
joonanyl <joona.nylander@metropolia.fi>
2022-02-04 11:08:01 +0200

    hibis

---
+++ src/main/java/r8/Main.java
@@ -0,0 +1,12 @@
+package r8;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SesFac sessari = new SesFac();
+        Postinumero postinumero = new Postinumero("asddddddddddddddddddd", "11111");
+
+        sessari.createPostinumero(postinumero);
+        sessari.Finalize();
+    }
+}
