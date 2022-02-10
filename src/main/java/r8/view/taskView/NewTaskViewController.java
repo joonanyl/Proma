a008e49639e97c9d6019a3623005032d596a121d
TeemuTallskog <72005659+TeemuTallskog@users.noreply.github.com>
2022-02-10 15:38:29 +0200

    Added controlsFX library, and made changes to create task view controller

--- src/main/java/r8/view/taskView/NewTaskViewController.java
+++ src/main/java/r8/view/taskView/NewTaskViewController.java
@@ -1,4 +1,49 @@
 package r8.view.taskView;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import org.controlsfx.control.textfield.TextFields;
+
+import javax.swing.*;
+import java.util.LinkedList;
+
 public class NewTaskViewController {
+    /*
+    @FXML
+    private JComboBox<User> comboBoxUser;
+
+    @FXML
+    private JComboBox<Team> comboBoxTeam;
+
+
+    public void initialize(){
+        comboBoxTeam.setEditable(true);
+        comboBoxUser.setEditable(true);
+
+        TextFields.bindAutoCompletion(comboBoxTeam.getEditor(), getTeams(comboBoxTeam));
+        TextFields.bindAutoCompletion(comboBoxUser.getEditor(), getUsers(comboBoxUser));
+
+    }
+
+
+    private Team[] getTeams(JComboBox comboBox){
+        int n = comboBox.getItemCount();
+        LinkedList<Team> arr = new LinkedList<Team>();
+        for(int i = 0; i < n; i++){
+            arr.add(comboBoxTeam.getItemAt(i));
+        }
+        return arr;
+    }
+
+    private User[] getUsers(JComboBox comboBox){
+        int n = comboBox.getItemCount();
+        LinkedList<User> arr = new LinkedList<User>();
+        for(int i = 0; i < n; i++){
+            arr.add(comboBoxUser.getItemAt(i));
+        }
+        return arr;
+    }
+
+     */
 }
