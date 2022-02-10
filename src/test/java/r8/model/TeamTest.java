6bae94ab6319cab637b2ea9b9128206dd04efbd7
sanku <sanna.kukkonen2@metropolia.fi>
2022-02-10 10:42:12 +0200

    TeamTest-testitiedoston tynkä

---
+++ src/test/java/r8/model/TeamTest.java
@@ -0,0 +1,49 @@
+package r8.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TeamTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testTeam() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTeamName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetTeamName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddMember() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddMembers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetMembers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
