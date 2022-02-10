6380b6905b6801807d85c9e2efb06ae622203d3c
TeemuTallskog <72005659+TeemuTallskog@users.noreply.github.com>
2022-02-10 14:26:22 +0200

    new task view updates

---
+++ src/main/java/r8/view/taskView/TaskViewController.java
@@ -0,0 +1,11 @@
+package r8.view.taskView;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+import r8.view.navigation.GetView;
+
+import java.io.IOException;
+
+public class TaskViewController {
+
+}
