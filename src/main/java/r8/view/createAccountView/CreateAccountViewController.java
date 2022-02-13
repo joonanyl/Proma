f1f47b71385b584fdee673d4977bceb5ac28f0a1
Aarni-Joakim <Aarni.Pesonen@metropolia.fi>
2022-02-13 15:15:17 +0200

    Added specific views for project, sprint, team, account specific tasks and create team.

--- src/main/java/r8/view/createAccountView/CreateAccountViewController.java
+++ src/main/java/r8/view/createAccountView/CreateAccountViewController.java
@@ -2,22 +2,14 @@ package r8.view.createAccountView;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
-import r8.view.navigation.GlobalControllerRef;
+import r8.view.navigation.GlobalControllerReference;
 
 import java.io.IOException;
-import java.util.Objects;
 
 public class CreateAccountViewController {
 
     @FXML
     private void navigate(ActionEvent event) throws IOException {
-        GlobalControllerRef.getInstance().getLoginViewController().handleNavigation(event);
+        GlobalControllerReference.getInstance().getLoginViewController().handleNavigation(event);
     }
 }
