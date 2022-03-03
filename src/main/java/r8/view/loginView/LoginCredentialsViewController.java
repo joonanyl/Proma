420fd60869b21a0f5164515624f52d805810960a
Aarni-Joakim <Aarni.Pesonen@metropolia.fi>
2022-03-03 11:02:40 +0200

    Removed IController used for testing. Added breadcrumb tweaks and stage is now fullscreen by default.

--- src/main/java/r8/view/loginView/LoginCredentialsViewController.java
+++ src/main/java/r8/view/loginView/LoginCredentialsViewController.java
@@ -2,17 +2,10 @@ package r8.view.loginView;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 import r8.App;
 import r8.view.navigation.GlobalControllerReference;
-
 import java.io.IOException;
-import java.util.Objects;
+
 
 public class LoginCredentialsViewController {
 
