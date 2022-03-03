3f29547704ebab497adb9b813db31c02d291134b
TeemuTallskog <72005659+TeemuTallskog@users.noreply.github.com>
2022-03-03 17:27:09 +0200

    Textfield validator and create task page functionality

--- src/main/java/r8/view/taskView/NewTaskViewController.java
+++ src/main/java/r8/view/taskView/NewTaskViewController.java
@@ -1,25 +1,59 @@
 package r8.view.taskView;
 
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import org.controlsfx.control.SearchableComboBox;
 import org.controlsfx.control.textfield.TextFields;
 import r8.model.Account;
 import r8.model.Team;
+import r8.model.TextFieldValidator;
+import r8.model.task.Task;
+import r8.model.task.TaskState;
+import r8.model.task.TaskType;
 
 import javax.swing.*;
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 public class NewTaskViewController {
 
     @FXML
-    private ComboBox<Account> comboBoxUser;
+    private SearchableComboBox<Account> comboBoxUser;
 
     @FXML
-    private ComboBox<Team> comboBoxTeam;
+    private SearchableComboBox<Team> comboBoxTeam;
+
+    @FXML
+    private TextField taskName;
+
+    @FXML
+    private ComboBox<TaskType> taskType;
+
+    @FXML
+    private TextArea descField;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private Button btnCancel;
+
+    @FXML
+    private Button btnAssignUser;
+
+    @FXML
+    private ListView<Account> listViewAssignedTo;
+
+    @FXML
+    private Button btnRemoveAssigned;
+
 
 
     public void initialize(){
+        taskType.getItems().setAll(TaskType.values());
+
         Account acc1 = new Account("Teemu", "Tallskog", "000", "email");
         Account acc2 = new Account("Teemua", "Tallskoga", "0001", "email");
         Account acc3 = new Account("Teemub", "Tallskogb", "0002", "email");
@@ -27,12 +61,48 @@ public class NewTaskViewController {
         comboBoxUser.getItems().add(acc1);
         comboBoxUser.getItems().add(acc2);
         comboBoxUser.getItems().add(acc3);
-
-        comboBoxTeam.setEditable(true);
-        comboBoxUser.setEditable(true);
-
-        TextFields.bindAutoCompletion((TextField) comboBoxTeam.getEditor(), comboBoxTeam.getItems());
-        TextFields.bindAutoCompletion((TextField) comboBoxUser.getEditor(), comboBoxUser.getItems());
+        TextFieldValidator textFieldValidator = new TextFieldValidator();
+        textFieldValidator.setValidation(taskName, "([A-Za-z0-9 ]{1,20})");
 
     }
+
+
+
+    @FXML
+    private void saveTask(){
+        if(!taskName.getText().matches("([A-Za-z0-9 ]{1,20})")){
+            System.out.println("didn't match");
+            return;
+        }
+        Task newTask = new Task(taskName.getText(), TaskState.NOT_STARTED, taskType.getValue());
+        newTask.setDescription(descField.getText());
+        ArrayList<Account> accountArrayList = new ArrayList<Account>();
+        if(listViewAssignedTo.getItems() != null){
+            accountArrayList.addAll(listViewAssignedTo.getItems());
+        }
+    }
+
+    @FXML
+    private void AssignUser(){
+        Account account = comboBoxUser.getSelectionModel().getSelectedItem();
+        if(account != null){
+            System.out.println(account.getFirstName());
+            listViewAssignedTo.getItems().add(account);
+            System.out.println("Added to list");
+            Platform.runLater(() -> {
+                comboBoxUser.getSelectionModel().clearSelection();
+                comboBoxUser.getItems().remove(account);
+            });
+        }
+    }
+
+    @FXML
+    private void removeAssigned(){
+        Account acc = listViewAssignedTo.getSelectionModel().getSelectedItem();
+        if(acc != null){
+            listViewAssignedTo.getItems().remove(acc);
+            comboBoxUser.getItems().add(acc);
+        }
+    }
+
 }
