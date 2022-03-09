231a2e064a1b8803e82297d7ba34e34e20c00d3c
Aarni-Joakim <Aarni.Pesonen@metropolia.fi>
2022-03-09 20:35:53 +0200

    Added Subview functionality for project view

--- src/main/java/r8/view/projectView/ProjectViewController.java
+++ src/main/java/r8/view/projectView/ProjectViewController.java
@@ -2,14 +2,18 @@ package r8.view.projectView;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import r8.App;
 import r8.model.appState.AppState;
 import r8.model.appState.IAppStateMain;
+import r8.view.navigation.GetView;
 
 import java.io.IOException;
 
@@ -60,12 +64,42 @@ public class ProjectViewController {
     @FXML
     private VBox vboxProjectTasks;
 
+    @FXML
+    private BorderPane projectSubViewPane;
+
     final IAppStateMain appStateMain = AppState.getInstance();
+    private String currentSubview;
+    private App app;
     private boolean admin;
 
-    public void initialize() {
+    private void initialize() throws IOException {
         admin = appStateMain.getIsAdmin();
         adminVisibility(admin);
+        handleNavigation("sprint-subview");
+    }
+
+    public void setMainApp(App app) {
+        this.app = app;
+    }
+
+    @FXML
+    public void handleNavigation(ActionEvent event) throws IOException {
+        GetView viewLoader = new GetView();
+        final Node eventSource = (Node) event.getSource();
+        String userData = (String) eventSource.getUserData();
+
+        if (!userData.equals(currentSubview)) {
+            System.out.println("Clicked " + userData);
+            projectSubViewPane.setCenter(viewLoader.getView(userData));
+            currentSubview = userData;
+        }
+    }
+
+    @FXML
+    public void handleNavigation(String viewName) throws IOException {
+        GetView viewLoader = new GetView();
+        projectSubViewPane.setCenter(viewLoader.getView(viewName));
+        currentSubview = viewName;
     }
 
     @FXML
