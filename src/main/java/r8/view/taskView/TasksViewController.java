a42885d4add90af1c74150d487ca3ab6377e97ca
Aarni-Joakim <Aarni.Pesonen@metropolia.fi>
2022-03-10 16:58:25 +0200

    Added tasks-view and controller

--- src/main/java/r8/view/taskView/TasksViewController.java
+++ src/main/java/r8/view/taskView/TasksViewController.java
@@ -2,6 +2,9 @@ package r8.view.taskView;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.HBox;
 import r8.model.appState.AppState;
 import r8.model.appState.IAppStateMain;
 
@@ -10,6 +13,24 @@ import java.io.IOException;
 public class TasksViewController {
 
     final IAppStateMain appStateMain = AppState.getInstance();
+
+    @FXML
+    private Button btnActiveTasks;
+
+    @FXML
+    private Button btnCompletedTasks;
+
+    @FXML
+    private Button btnGotToTaskView;
+
+    @FXML
+    private Button buttonAllTasks;
+
+    @FXML
+    private ListView<?> listViewMyTasks;
+
+    @FXML
+    private HBox projectNavBar;
 
     @FXML
     private void navigate(ActionEvent event) throws IOException {
