e998035e987f6e4d8d7f6fe33f36adc0b0965aca
Joona Nylander <joona.nylander@metropolia.fi>
2022-03-25 17:42:22 +0200

    Task - Team association

--- src/main/java/r8/model/dao/TaskDAO.java
+++ src/main/java/r8/model/dao/TaskDAO.java
@@ -1,9 +1,15 @@
 package r8.model.dao;
 
 import org.hibernate.HibernateException;
+import r8.model.Account;
+import r8.model.Project;
+import r8.model.Team;
 import r8.model.task.Task;
+import r8.model.task.TaskType;
 
+import javax.persistence.Entity;
 import javax.persistence.EntityManager;
+import java.util.List;
 
 public class TaskDAO {
     private EntityManager entityManager;
@@ -27,6 +33,79 @@ public class TaskDAO {
         try {
             return entityManager.find(Task.class, taskId);
         } catch (NullPointerException e) {
+            return null;
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    public List<Task> getAll() {
+        entityManager = DAOUtil.getEntityManager();
+        try {
+             return entityManager.createQuery("SELECT t FROM Task t", Task.class)
+                     .getResultList();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    public List<Task> getByTeam(Team team) {
+        entityManager = DAOUtil.getEntityManager();
+        try {
+            return entityManager.createQuery(
+                    "SELECT t FROM Task t JOIN t.teams tm WHERE tm.teamId = :teamId", Task.class)
+                    .setParameter("teamId", team.getTeamId())
+                    .getResultList();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    public List<Task> getByProject(Project project) {
+        entityManager = DAOUtil.getEntityManager();
+        try {
+            return entityManager.createQuery(
+                    "SELECT t FROM Task t WHERE t.project = :project", Task.class)
+                    .setParameter("project", project)
+                    .getResultList();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    public List<Task> getByAccount(Account account) {
+        entityManager = DAOUtil.getEntityManager();
+        try {
+            return entityManager.createQuery(
+                    "SELECT t FROM Task t join t.accounts a WHERE a.accountId = :accountId", Task.class)
+                    .setParameter("accountId", account.getAccountId())
+                    .getResultList();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    public List<Task> getByTaskType(TaskType taskType) {
+        entityManager = DAOUtil.getEntityManager();
+        try {
+            return entityManager.createQuery(
+                    "SELECT t FROM Task t WHERE t.taskType = :taskType", Task.class)
+                    .setParameter("taskType", taskType)
+                    .getResultList();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
             return null;
         } finally {
             entityManager.close();
@@ -61,4 +140,37 @@ public class TaskDAO {
         }
     }
 
+    public void assignToTeam(Team team, Task task) {
+        entityManager = DAOUtil.getEntityManager();
+        team = entityManager.contains(team) ? team : entityManager.merge(team);
+        task = entityManager.contains(task) ? task : entityManager.merge(task);
+
+        try {
+            entityManager.getTransaction().begin();
+            task.assignToTeam(team);
+            entityManager.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            entityManager.getTransaction().rollback();
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    public void removeTeamAssociation(Team team, Task task) {
+        entityManager = DAOUtil.getEntityManager();
+        team = entityManager.contains(team) ? team : entityManager.merge(team);
+        task = entityManager.contains(task) ? task : entityManager.merge(task);
+
+        try {
+            entityManager.getTransaction().begin();
+            task.removeFromTeam(team);
+            entityManager.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            entityManager.getTransaction().rollback();
+        } finally {
+            entityManager.close();
+        }
+    }
 }
