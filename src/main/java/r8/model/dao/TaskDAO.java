55003f6d7bf35c7120ffe8fc403b829555958898
Joona Nylander <joona.nylander@metropolia.fi>
2022-03-25 17:28:59 +0200

    More DAO changes

--- src/main/java/r8/model/dao/TaskDAO.java
+++ src/main/java/r8/model/dao/TaskDAO.java
@@ -1,5 +1,6 @@
 package r8.model.dao;
 
+import org.hibernate.HibernateException;
 import r8.model.task.Task;
 
 import javax.persistence.EntityManager;
@@ -7,32 +8,57 @@ import javax.persistence.EntityManager;
 public class TaskDAO {
     private EntityManager entityManager;
 
-    public TaskDAO() {
-        this.entityManager = DAOUtil.getEntityManager();
-    }
-
     public void persist(Task task) {
-        entityManager.getTransaction().begin();
-        entityManager.persist(task);
-        entityManager.getTransaction().commit();
+        entityManager = DAOUtil.getEntityManager();
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(task);
+            entityManager.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            entityManager.getTransaction().rollback();
+        } finally {
+            entityManager.close();
+        }
     }
 
     public Task get(int taskId) {
-        Task task = entityManager.getReference(Task.class, taskId);
-        entityManager.detach(task);
-        return task;
+        entityManager = DAOUtil.getEntityManager();
+        try {
+            return entityManager.find(Task.class, taskId);
+        } catch (NullPointerException e) {
+            return null;
+        } finally {
+            entityManager.close();
+        }
     }
 
     public void update(Task task) {
-        entityManager.getTransaction().begin();
-        entityManager.merge(task);
-        entityManager.getTransaction().commit();
+        entityManager = DAOUtil.getEntityManager();
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.merge(task);
+            entityManager.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            entityManager.getTransaction().rollback();
+        } finally {
+            entityManager.close();
+        }
     }
 
     public void remove(Task task) {
-        entityManager.getTransaction().begin();
-        entityManager.remove(task);
-        entityManager.getTransaction().commit();
+        entityManager = DAOUtil.getEntityManager();
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.remove(task);
+            entityManager.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            entityManager.getTransaction().rollback();
+        } finally {
+            entityManager.close();
+        }
     }
 
 }
