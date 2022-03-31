08106f447901a5038a7e24dc8be0e6f518a7d5e2
sebastml <sebastian.lundin@metropolia.fi>
2022-03-31 13:00:44 +0300

    new TextLoader class and properties for localization. Not yet functional

---
+++ src/main/java/r8/util/TextLoader.java
@@ -0,0 +1,63 @@
+package r8.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * A class for retrieving String objects used for localization from text resource files.
+ */
+public class TextLoader {
+    private static TextLoader INSTANCE;
+    private ResourceBundle bundle;
+    private Locale locale;
+
+    private TextLoader() {}
+
+    /**
+     * Gets the TextLoader instance
+     * @return Instance of TextLoader
+     */
+    public static TextLoader getInstance() {
+        if (TextLoader.INSTANCE == null) {
+            TextLoader.INSTANCE = new TextLoader();
+        }
+        return TextLoader.INSTANCE;
+    }
+
+    private void initLoader() {
+        if (locale == null) {
+            System.err.println("Locale object not set in TextLoader.java");
+            return;
+        }
+        try {
+            bundle = ResourceBundle.getBundle("TextResources", locale);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Retrieves the resource specified by String name that should point to a key in a properties file
+     * @param name is the key to a property
+     * @return String associated with the property key
+     */
+    public String getResource(String name) {
+        if (bundle == null) {
+            initLoader();
+        }
+        return bundle.getString(name);
+    }
+
+    /**
+     * Sets the current locale object to be used for retrieving the correct resource bundle
+     * @param language String for locale object constructor language parameter
+     * @param country String for locale object constructor country parameter
+     */
+    public void setLocale(String language, String country) {
+        this.locale = new Locale(language, country);
+    }
+}
---
+++ src/test/java/r8/util/TextLoaderTest.java
@@ -0,0 +1,24 @@
+package r8.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class TextLoaderTest {
+    @BeforeAll
+    static void setLocale() {
+        TextLoader.getInstance().setLocale("fi", "FI");
+    }
+
+    @Test
+    void isSingleton() {
+        assertEquals(TextLoader.getInstance().hashCode(), TextLoader.getInstance().hashCode());
+    }
+
+    @Test
+    void retrievesString() {
+        assertTrue(TextLoader.getInstance().getResource("appTitle").equals("Proma - Project Manager"));
+    }
+}
