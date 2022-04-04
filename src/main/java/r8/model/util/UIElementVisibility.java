e514303cfe6a348e81909c1015c4cd14dcbad1d4
Aarni-Joakim <Aarni.Pesonen@metropolia.fi>
2022-04-04 21:02:01 +0300

    Added new team-view with admin management and visibility functionality + styling

--- src/main/java/r8/model/util/UIElementVisibility.java
+++ src/main/java/r8/model/util/UIElementVisibility.java
@@ -1,12 +1,19 @@
 package r8.model.util;
 
 import javafx.scene.layout.Pane;
+import r8.model.Account;
 
 public class UIElementVisibility {
 
     public Pane toggleVisibility(Pane pane) {
         pane.setVisible(!pane.isVisible());
         pane.setManaged(!pane.isManaged());
+        return pane;
+    }
+
+    public Pane toggleAdminVisibility(Pane pane, boolean admin) {
+        if(admin) { pane.setVisible(true); pane.setManaged(true); };
+        if(!admin) { pane.setVisible(false); pane.setManaged(false); };
         return pane;
     }
 }
