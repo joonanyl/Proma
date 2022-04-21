17f4f896d24ee4063520dc8b536ea6ec41b3e229
sanku <sanna.kukkonen2@metropolia.fi>
2022-04-21 09:39:47 +0300

    välikommitti

---
+++ src/test/java/r8/model/dao/ProjectDAOTest.java
@@ -0,0 +1,43 @@
+package r8.model.dao;
+
+import org.junit.jupiter.api.*;
+import r8.model.Project;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class ProjectDAOTest {
+
+    private static ProjectDAO projectDAO;
+    private static Project project1, project2, project3, project4;
+
+    @BeforeAll
+    static void setUpBeforeTesting() {
+        projectDAO = new ProjectDAO();
+        project1 = new Project("Project1", "desc1");
+        project2 = new Project("Project2", "desc2");
+        project3 = new Project("Project3", "desc3");
+        project4 = new Project("Project4", "desc4");
+
+    }
+
+    @Test
+    @Order(1)
+    void persist(){
+
+    }
+
+
+
+    @AfterEach
+    void clearDatabase(){
+        projectDAO.remove(project1);
+        projectDAO.remove(project2);
+        projectDAO.remove(project3);
+        projectDAO.remove(project4);
+        System.out.println("db cleared");
+    }
+
+}
---
+++ src/test/java/r8/model/dao/SprintDAOTest.java
@@ -0,0 +1,77 @@
+package r8.model.dao;
+
+import org.junit.jupiter.api.*;
+import r8.model.Project;
+import r8.model.Sprint;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class SprintDAOTest {
+/* KOMMENTOITU TESTIKSI POIS
+    private static Project project;
+    private static Sprint sprint;
+    private static SprintDAO sprintDAO;
+    private static ProjectDAO projectDAO;
+
+    @BeforeAll
+    static void setUpBeforeTesting() {
+        project = new Project("project name ", "desc");
+        sprint = new Sprint("sprint", LocalDate.now(), LocalDate.now().plusDays(14), project);
+        sprintDAO = new SprintDAO();
+        projectDAO = new ProjectDAO();
+        projectDAO.persist(project);
+    }
+
+    @Test
+    @Order(1)
+    void addAndGetSprint() {
+        sprintDAO.persist(sprint);
+        assertEquals(sprint, sprintDAO.get(sprint.getSprintId()), "Sprintin hakeminen tietokannasta epäonnistui");
+    }
+
+    @Test
+    @Order(2)
+    void update() {
+        String oldName = sprint.getName();
+        sprint.setName("new name");
+        sprintDAO.update(sprint);
+        Sprint sprintFromDB = sprintDAO.get(sprint.getSprintId());
+
+        assertEquals(sprint.getName(), sprintFromDB.getName(), "Tietokannasta haettu sprint ei täsmää");
+        assertNotEquals(oldName, sprintFromDB.getName(), "Sprintin nimen päivittääminen tietokannassa epäonnistui");
+    }
+
+    @Test
+    @Order(3)
+    void removeAndGetAll() {
+        Sprint sprint2 = new Sprint("sprint2", LocalDate.now(), LocalDate.now().plusDays(14), project);
+
+        sprintDAO.persist(sprint2);
+
+        List<Sprint> dbList = sprintDAO.getAll();
+
+        assertTrue(dbList.contains(sprint2), "Sprint2 ei löydy tietokannan listasta");
+
+        sprintDAO.remove(sprint2);
+        dbList = sprintDAO.getAll();
+
+        assertFalse(dbList.contains(sprint2), "Sprint2 ei poistunut tietokannan listasta");
+
+    }
+
+    @AfterAll
+    static void clearDatabase(){
+        sprintDAO.remove(sprint);
+        projectDAO.removeProjectById(project.getProjectId());
+        System.out.println("DB cleared");
+
+    }
+    */
+
+}
