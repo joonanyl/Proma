69ba40a1fd6e1845d9c42f105e8e70da2085d10f
Aarni-Joakim <Aarni.Pesonen@metropolia.fi>
2022-05-04 16:35:28 +0300

    JavaDoc grind

--- src/main/java/r8/controller/IControllerAccount.java
+++ src/main/java/r8/controller/IControllerAccount.java
@@ -2,8 +2,16 @@ package r8.controller;
 
 import r8.model.Account;
 
+/**
+ * Interface for {@link Controller} with account specific methods
+ * @author Aarni Pesonen
+ */
 public interface IControllerAccount {
 
+    /**
+     * Returns
+     * @return
+     */
     Account getAccount();
     boolean authenticatePassword(String password);
     void updateAccount(String firstName, String lastName, String email, String password);
--- src/main/java/r8/model/appState/IAppStateLogin.java
+++ src/main/java/r8/model/appState/IAppStateLogin.java
@@ -2,12 +2,40 @@ package r8.model.appState;
 
 import r8.view.loginView.LoginViewController;
 
+/**
+ * Interface for {@link AppState} used in conjunction with login specific views
+ * Only contains {@link AppState} methods appropriate for login specific tasks
+ * @author Aarni Pesonen
+ */
 public interface IAppStateLogin {
 
+    /**
+     * Used when a new account is created
+     * requires user input for
+     * @param firstName as a string
+     * @param lastName as a string
+     * @param email as a string in email format
+     * @param password as a string with specific requirements
+     */
     void createAccount(String firstName, String lastName, String email, String password);
+
+    /**
+     * For authenticating user logic with bCrypt
+     * @param email used as an account identifier
+     * @param password needs to match with account and existing hash information
+     * @return boolean value indicating the success of the login operation
+     */
     boolean authenticateLogin(String email, String password);
 
+    /**
+     * Used by login view to get active {@link LoginViewController} used for subview navigation
+     * @return currently active {@link LoginViewController}
+     */
     LoginViewController getLoginViewController();
 
+    /**
+     * Used by login view to set active {@link LoginViewController} used for subview navigation
+     * @param loginViewController currently active
+     */
     void setLoginViewController(LoginViewController loginViewController);
 }
